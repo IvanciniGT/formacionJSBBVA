6a1cae342407696d254cc782811babe609ee6a09
Ubuntu <ubuntu@ip-172-31-26-166.eu-west-1.compute.internal>
2023-01-03 14:57:47 +0000

    inicio dia 2

--- juegoFrioCaliente/src/juegos/ahorcado/juego-ahorcado.js
+++ juegoFrioCaliente/src/juegos/ahorcado/juego-ahorcado.js
@@ -1,26 +1,49 @@
 const Juego = require("../../juego");
 var utilidadesAhorcado = require('./utilidades');
+const estadoPartida = require("../../estado-partida");
 
 class JuegoAhorcado extends Juego {
     
-    constructor(){
+    constructor(fichero){
         // Elegir una palabra aleatoriamente de una colección
+        this.palabra = utilidadesAhorcado.elegirPalabra(fichero);
         // Preparar una estructura adecuada de representación de datos... para juagr
-        this.estructura = utilidadesAhorcado.prepararEstructuraInicial(palabra);
+        this.estructura = utilidadesAhorcado.prepararEstructuraInicial(this.palabra);
+        this.pendientes = this.palabra.length()
     }
     
     jugar(){
+        var vidas = 6;
+        var acertado = false;
         // saludar al usuario y explicarle de qué va esto
+        this.mensajeBienvenida();
+        
         // Mientras le queden vidas y no haya acertado:
+        while (vidas >=0 && ! acertado){
             // mostrar Información relevante: La palabra enmascarada, intentos que le quedan
+            this.mensajeBienvenida();
             // Pedirle una letra
+            const letra = this.pedirLetra();
             // Comprobar la letra -> Boolean
+            const resultado = utilidadesAhorcado.comprobarNuevaLetra(letra,this.estructura, this.pendientes);
+            this.estructura = resultado.estructura
+            this.pendientes = resultado.pendientes
             // Ha acertado la letra: 
+            if (resultado.encontrada){
             // Si si? Si ya tiene todo descubierto?
-            //          Si si? Partida acabada
-            //        Si no? Otra ronda
+                if (resultado.pendientes == 0)
+                    //          Si si? Partida acabada
+                    return estadoPartida.GANADA;
+                else
+                    //        Si no? Otra ronda
+                    continue
+            }else{    
+                vidas-=1;
+            }
             // Si no? 
             //  Descuento vida y a por otro intento
+        }
+        return estadoPartida.PERDIDA;
     }
     
     
